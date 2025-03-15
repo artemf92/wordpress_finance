4233124d5970f4da5c1d996b7c3ee5ae8eeb62b3
Artem Frolov <fa92@live.ru>
2025-03-15 23:18:18 +0200

    Настроил фильтры статистики для менеджеров, админов и бухгалтеров.

--- wp-content/themes/finansist/template-parts/analytics/content-statistics.php
+++ wp-content/themes/finansist/template-parts/analytics/content-statistics.php
@@ -1,20 +1,18 @@
 <section class="statistics">
   <? 
-  if (current_user_can('manager')) {
+  if (current_user_can('manager') || current_user_can('accountant') || current_user_can('administrator')) {
     get_template_part('template-parts/admin-group/filter', 'groups');
-    get_template_part('template-parts/admin-group/filter', 'date');
   }
 
   $filterDate = "";
   $filterGroup = "";
   $filterType = "";
 
-  if (current_user_can('accountant')) {
+  if (current_user_can('accountant') || current_user_can('manager') || current_user_can('administrator')) {
     $filterType = " type=\"accountant\"";
 
-    if (current_user_can('manager')) {
+    if (current_user_can('manager') || current_user_can('administrator')) {
       $filterGroup = isset($_GET['group']) ? explode(',', $_GET['group']):get_user_meta(get_current_user_id(), 'pm_group', true);
-      $filterDate = isset($_GET['date']) ? " date=\"".$_GET['date']."\"":"";
     } else {
       $filterGroup = get_user_meta(get_current_user_id(), 'pm_group', true);
     }
@@ -24,11 +22,13 @@
     }
 
     echo '<h2>' . __('Статистика') . '</h2>';
+    $filterDate = isset($_GET['date']) ? " date=\"".$_GET['date']."\"":"";
     
     foreach($filterGroup as $gid) {
       echo do_shortcode("[statistics gid=\"{$gid}\"{$filterType}{$filterDate}]") ;
     }
   // } else if (current_user_can('project_manager')) {
   }
+
   ?>
 </section>
